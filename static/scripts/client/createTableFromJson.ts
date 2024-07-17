45bc075849aebff588ba24d0b12a379b4b485e51
MadOtacu <pipboy1917@gmail.com>
2024-07-17 18:58:29 +0400

    добавлены иконки типов файлов и улучшено взаимодействие с системой

--- static/scripts/client/createTableFromJson.ts
+++ static/scripts/client/createTableFromJson.ts
@@ -7,12 +7,6 @@ export function createTableFromJson (response: any) {
 
     let table = document.createElement("table")
 
-    const fileImg = new Image()
-    const directoryImg = new Image()
-
-    fileImg.src = fileImgLink
-    directoryImg.src = directoryImgLink
-
     for (let rowi = 0; rowi < response.length; rowi++) {
         let tr = table.insertRow(rowi)
 
@@ -20,13 +14,21 @@ export function createTableFromJson (response: any) {
         let tabName = tr.insertCell(1)
         let tabSize = tr.insertCell(2)
 
+        tabName.classList.add("tabWide")
+        tabType.classList.add("tabShort")
+        tabSize.classList.add("tabShort")
+
         tabName.textContent = response[rowi].name
         tabSize.textContent = response[rowi].convertedSize
 
         if (response[rowi].fileType == "Файл") {
+            const fileImg = new Image()
+            fileImg.src = fileImgLink
             tabType.appendChild(fileImg)
         }
         if (response[rowi].fileType == "Директория") {
+            const directoryImg = new Image()
+            directoryImg.src = directoryImgLink
             tabType.appendChild(directoryImg)
             tr.addEventListener("click", dirDown)
             tr.classList.add("buttonCell")
